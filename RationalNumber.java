856c93e8b76f5790d10f6ef7264ea42df89efebe
pokiebob <ccursetjee@gmail.com>
2020-11-22 21:02:14 -0500

    constructor

---
+++ RationalNumber.java
@@ -0,0 +1,61 @@
+public class RationalNumber extends RealNumber {
+  private int numerator, denominator;
+
+  /**Initialize the RationalNumber with the provided values
+  *  if the denominator is 0, make the fraction 0/1 instead
+  *@param nume the numerator
+  *@param deno the denominator
+  */
+  public RationalNumber(int nume, int deno){
+    super(0.0);
+    if (deno == 0) {
+      numerator = 0;
+      denominator = 1;
+    } else {
+      numerator = nume;
+      denominator = deno;
+    }
+  }
+
+  /**
+  *Divide the numerator and denominator by the GCD
+  *This must be used to maintain that all RationalNumbers are
+  *reduced after construction.
+  */
+  private void reduce(){
+
+  }
+  /******************Operations Return a new RationalNumber!!!!****************/
+  /**
+  *Return a new RationalNumber that is the product of this and the other
+  */
+  public RationalNumber multiply(RationalNumber other){
+    return null;
+  }
+
+  /**
+  *Return a new RationalNumber that is the this divided by the other
+  */
+  public RationalNumber divide(RationalNumber other){
+    return null;
+  }
+
+  /**
+  *Return a new RationalNumber that is the sum of this and the other
+  */
+  public RationalNumber add(RationalNumber other){
+    return null;
+  }
+  /**
+  *Return a new RationalNumber that this minus the other
+  */
+  public RationalNumber subtract(RationalNumber other){
+    return null;
+  }
+
+  public static void main(String[] args) {
+    RationalNumber a = new RationalNumber(1, 2);
+    RationalNumber b = new RationalNumber(4, 1);
+    RationalNumber c = new RationalNumber(20, 0);
+  }
+} 
